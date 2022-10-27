e3eb9288a8b47439a6b1223df43567a03c2464d0
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2022-10-27 16:39:56 -0700

    implement queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,38 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    private head: QNode<T> | undefined;
+    private tail: QNode<T> | undefined;
 
     enqueue(item: T): void {
+        const prevTail = this.tail;
+        this.tail = {
+            value: item,
+        };
+        if (prevTail) {
+            prevTail.next = this.tail;
+        }
+        if (!this.head) {
+            this.head = this.tail;
+        }
+        this.length++;
+    }
 
-}
     deque(): T | undefined {
+        if (this.head) {
+            this.length--;
+        }
+        const val = this.head?.value;
+        this.head = this.head?.next;
+        return val;
+    }
 
-}
     peek(): T | undefined {
+        return this.head?.value;
+    }
 
-}
 }
